f1f0ab6fb145c32571ceee0e0d84ce8bc367fe37
Patricia Moro <patricialmoro@gmail.com>
2025-05-10 22:40:14 +0100

    Inicia repositório do zero

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,39 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    <Part name={parts[0].name} exercises={parts[0].exercises} />
+    <Part name={parts[1].name} exercises={parts[1].exercises} />
+    <Part name={parts[2].name} exercises={parts[2].exercises} />
+  </div>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = 'Desenvolvimento de aplicação Half Stack';
+  const parts = [
+    { name: 'Fundamentos da biblioteca React', exercises: 10 },
+    { name: 'Usando props para passar dados', exercises: 7 },
+    { name: 'Estado de um componente', exercises: 14 }
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
